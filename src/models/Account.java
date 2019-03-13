87db185361aeda9c053c9b7b99132f35939cf326
Diego3799 <diego3799@gmail.com>
2019-03-13 08:32:24 -0600

    Constructor of client

---
+++ src/models/Account.java
@@ -0,0 +1,13 @@
+package models;
+
+public class Account {
+    public Account(){
+        int password;
+        int numCard;
+        String user;
+    }
+    public 
+
+
+
+}
---
+++ src/models/Client.java
@@ -0,0 +1,7 @@
+package models;
+
+public class Client {
+    private  String name;
+    Account client= new Account();
+
+}
