33d931313753623bcde6dd7164b4f54128e7331a
Sebastian <Hernandez>
2019-03-19 21:54:52 -0600

    Diseños agregados, aun faltan

---
+++ src/ui/Prueba.java
@@ -0,0 +1,96 @@
+///*
+// * Created by JFormDesigner on Thu Mar 14 08:40:35 CST 2019
+// */
+//
+//package ui;
+//
+//import javax.swing.*;
+//
+///**
+// * @author SEBASTIAN HERNANDEZ
+// */
+//public class Prueba extends JPanel {
+//	public Prueba() {
+//		initComponents();
+//	}
+//
+//	private void initComponents() {
+//		// JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
+//		// Generated using JFormDesigner Evaluation license - SEBASTIAN HERNANDEZ
+//		panel2 = new JPanel();
+//		textField1 = new JTextField();
+//
+//		//======== this ========
+//
+//		// JFormDesigner evaluation mark
+//		setBorder(new javax.swing.border.CompoundBorder(
+//			new javax.swing.border.TitledBorder(new javax.swing.border.EmptyBorder(0, 0, 0, 0),
+//				"JFormDesigner Evaluation", javax.swing.border.TitledBorder.CENTER,
+//				javax.swing.border.TitledBorder.BOTTOM, new java.awt.Font("Dialog", java.awt.Font.BOLD, 12),
+//				java.awt.Color.red), getBorder())); addPropertyChangeListener(new java.beans.PropertyChangeListener(){public void propertyChange(java.beans.PropertyChangeEvent e){if("border".equals(e.getPropertyName()))throw new RuntimeException();}});
+//
+//		setLayout(new MigLayout(
+//			"hidemode 3,align left top",
+//			// columns
+//			"[fill]" +
+//			"[fill]" +
+//			"[fill]" +
+//			"[fill]" +
+//			"[fill]" +
+//			"[fill]" +
+//			"[fill]" +
+//			"[fill]" +
+//			"[fill]" +
+//			"[fill]" +
+//			"[fill]" +
+//			"[fill]" +
+//			"[fill]" +
+//			"[fill]",
+//			// rows
+//			"[]" +
+//			"[]" +
+//			"[]" +
+//			"[]" +
+//			"[]" +
+//			"[]" +
+//			"[]" +
+//			"[]" +
+//			"[]" +
+//			"[]" +
+//			"[]"));
+//
+//		//======== panel2 ========
+//		{
+//			panel2.setLayout(new MigLayout(
+//				"hidemode 3",
+//				// columns
+//				"[fill]" +
+//				"[fill]" +
+//				"[fill]",
+//				// rows
+//				"[]" +
+//				"[]" +
+//				"[]" +
+//				"[]" +
+//				"[]" +
+//				"[]" +
+//				"[]" +
+//				"[]" +
+//				"[]" +
+//				"[]" +
+//				"[]"));
+//
+//			//---- textField1 ----
+//			textField1.setText("usuario");
+//			panel2.add(textField1, "cell 0 1 2 1");
+//		}
+//		add(panel2, "cell 0 0");
+//		// JFormDesigner - End of component initialization  //GEN-END:initComponents
+//	}
+//
+//	// JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
+//	// Generated using JFormDesigner Evaluation license - SEBASTIAN HERNANDEZ
+//	private JPanel panel2;
+//	private JTextField textField1;
+//	// JFormDesigner - End of variables declaration  //GEN-END:variables
+//}
