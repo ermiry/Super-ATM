781a551510eea53497689296250e4f748d05f436
Erick Salas <stumpestro@ermiry.com>
2019-03-20 08:28:12 -0600

    Project reset

--- src/models/CheckAccount.java
+++
@@ -1,5 +0,0 @@
-package models;
-public class CheckAccount extends  Account {
-    boolean active=false;
-
-}
--- src/models/Client.java
+++
@@ -1,6 +0,0 @@
-package models;
-
-public class Client {
-    private  String name;
-    Account[] firstAccount = new Account[2];
-}
